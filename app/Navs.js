df67c603afc611e7d0827d754f979ab26c6c91f3
Rudolf Hersén <rudolf.hersen@schibsted.se>
2016-02-17 22:12:13 +0100

    clean up Navs

--- app/Navs.js
+++ app/Navs.js
@@ -5,32 +5,28 @@ const request = require('then-request')
 const StationLink = require('./StationLink')
 
 module.exports = React.createClass({
-    getClickHandler: function (station) {
-        const p = this.props
-
-        function handleClick() {
-            return function () {
-                request('GET', 'api/departures/' + station).done(function (res) {
-                    const message = JSON.parse(res.getBody())
-                    const trainAnnouncement = message.RESPONSE.RESULT[0].TrainAnnouncement
-                    p.setTrains(trainAnnouncement)
-                })
-            }
+    stationLink: function (station) {
+        const handleDepartures = (res) => {
+            const message = JSON.parse(res.getBody())
+            const trainAnnouncement = message.RESPONSE.RESULT[0].TrainAnnouncement
+            this.props.setTrains(trainAnnouncement)
         }
-
-        return <StationLink onClick={handleClick} name={this.props.names[station]} key={station}/>
+        return <StationLink
+            onClick={ () => { request('GET', 'api/departures/' + station).done(handleDepartures) } }
+            name={this.props.names[station]}
+            key={station}/>
     },
 
     render: function () {
         if (this.props.show) return (
             <div id="navs">
-                <nav className="pull-left">{this.props.stations.nw.map(this.getClickHandler)}</nav>
-                <nav className="pull-right">{this.props.stations.ne.map(this.getClickHandler)}</nav>
-                <nav className="center wide">{this.props.stations.c.map(this.getClickHandler)}</nav>
-                <nav className="pull-left">{this.props.stations.sw.map(this.getClickHandler)}</nav>
-                <nav className="pull-right">{this.props.stations.se.map(this.getClickHandler)}</nav>
+                <nav className="pull-left">{this.props.stations.nw.map(this.stationLink)}</nav>
+                <nav className="pull-right">{this.props.stations.ne.map(this.stationLink)}</nav>
+                <nav className="center wide">{this.props.stations.c.map(this.stationLink)}</nav>
+                <nav className="pull-left">{this.props.stations.sw.map(this.stationLink)}</nav>
+                <nav className="pull-right">{this.props.stations.se.map(this.stationLink)}</nav>
             </div>
         )
-        else return <div>Tillbaka</div>
+        else return <div id="navs" onClick={()=>{this.props.setTrains([])}}>Tillbaka</div>
     }
 })
